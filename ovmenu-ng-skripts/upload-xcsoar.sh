93a21109fea2919b745b925211efda72e3dde315
Caz Yokoyama <caz@caztech.com>
2022-02-05 12:13:03 -0800

    copy ovmenu-ng.sh and ovmenu-ng-skripts of 5f85d706c1744835db145129acc82c53a0bbbd61. scp meta-ov/recipes-apps/ovmenu-ng/files/ovmenu-ng.sh pi@192.168.0.197:~/ovmenu rsync -a meta-ov/recipes-apps/ovmenu-ng-skripts/files/ pi@192.168.0.197:~/ovmenu/ovmenu-ng-skripts/ chmod +x ovmenu-ng-skripts/*

---
+++ ovmenu-ng-skripts/upload-xcsoar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USB_PATH="/usb/usbstick/openvario/upload/xcsoar"
+UPLOAD_PATH="/home/root/.xcsoar"
+if [ -z "$(ls $USB_PATH/* 2>/dev/null)" ]; then
+        echo "No files found !!!"
+else
+        for uploadfile in $(find $USB_PATH -type f); do
+                echo $uploadfile
+                cp $uploadfile $UPLOAD_PATH
+        done
+fi
+echo "Umount Stick ..."
+sync
+umount /dev/sda1
+echo "Done !!"
